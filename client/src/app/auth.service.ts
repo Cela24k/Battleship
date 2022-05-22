ad3b4bb5c672168572ea366c29a0b3b3379a7bbf
Mimiview <57045037+Mimiview@users.noreply.github.com>
2022-05-22 18:59:36 +0200

    changed content type, restyled subscribe

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -16,18 +16,23 @@ export class AuthService {
       headers: new HttpHeaders({
         //authorization: 'Basic' + btoa( username+':'+email+':'+password),
         'cache-control': 'no-cache',
-        'Content-Type':  'application/x-www-form-urlencoded',
+        'Content-Type':  'application/json',
       }),
     };
-    let body = new URLSearchParams();// per riuscire a passare il body bisogna creare dei reqparams e passare body.toString
-    body.set('username', username);
-    body.set('password', password);
-    body.set('email', email);
+    // let body = new URLSearchParams();// per riuscire a passare il body bisogna creare dei reqparams e passare body.toString
+    // body.set('username', username);
+    // body.set('password', password);
+    // body.set('email', email);
+    let body = {
+      username ,
+      email,
+      password
+    }
     
     
     //debugger;
-    //questo passaggio va in errore, non arriva sta post
-    return this.http.post(this.url + '/register', body.toString(), options).pipe(
+    //l'errore dovrebbe essere dato dal tipo di ritorno del body della response, httpclient si aspetta un JSON qui spiega bene come funziona x-www-form-urlencoded https://stackoverflow.com/questions/4007969/application-x-www-form-urlencoded-or-multipart-form-data 
+    return this.http.post(this.url + '/register', body, options).pipe(
       tap( (data) => {
         console.log(JSON.stringify(data) + 'auth service');
       }));;
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -14,21 +14,27 @@ import { AuthService } from '../auth.service';
 export class RegisterComponent implements OnInit {
 
   public errmessage = undefined;
-  constructor(private auth: AuthService, private router:Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(username: string, email: string, password: string){
-    console.log('Submitting this info: ' + username +' '+ email +' '+password);
-    this.auth.register(username,email,password).subscribe( (d) => {// TODO error problem need to be checked
-      console.log('Login granted: ' + JSON.stringify(d) );
-      this.errmessage = undefined;
-      this.router.navigate(['/']);
-    }, (err) => {
-      console.log('Login error: ' + JSON.stringify(err) );
-      this.errmessage = err.message;
+  onSubmit(username: string, email: string, password: string) {
+    console.log('Submitting this info: ' + username + ' ' + email + ' ' + password);
+    this.auth.register(username, email, password).subscribe({
+      next: (d) => {// TODO error problem need to be checked
+        console.log('Login granted: ' + JSON.stringify(d));
+        this.errmessage = undefined;
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.log(err);
 
+        console.log('Login error: ' + JSON.stringify(err));
+        this.errmessage = err.message;
+
+      },
+      complete: ()=> console.log('Registration completedf'),
     });
   }
 }
