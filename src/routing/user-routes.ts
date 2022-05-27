d1b0ef8e85dfb31f5171c84399ecc6f37043cb47
Mimiview <57045037+Mimiview@users.noreply.github.com>
2022-05-27 17:33:38 +0200

    get users implemented with projection

--- src/routing/user-routes.ts
+++ src/routing/user-routes.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
 import * as user from '../models/user'
-import {Router} from "express";
+import { Router } from "express";
 var router = Router();
 
 /*
     ENDPOINTS	        ATTRIBUTES	    METHOD	    DESCRIPTION
     /users		                        GET	        Returns a list of all users if the client is a moderator
-			
+        	
     /users		                        POST	    Create a new user if the client is a moderator 
                                                     (Maybe replaced by /register or just use this endpoint in the register section)
 
@@ -17,30 +17,26 @@ var router = Router();
     
  **/
 
-router.get('/list', (req, res)=>{
+router.get('/list', (req, res) => {
     console.log("GIUSTO ED AUTENTICATO");//Debugging tool for auth
     res.send(202);
 })
 
-router.get('/', (req,res) => {
+router.get('/', async (req, res) => {
     //da prendere dal token idk
-    let moderator = true;
+    var projection = {
+        username: true,
+        friends: true,
+        stats: true,
+    }
 
-    if(moderator) 
-        return res.status(200).json({error: false})
-    else 
-        return res.status(400).json({error:true, erorrmessage:"Non sei autenticato"})
+    await user.getModel().find({}, projection).then(function (data) {
+        res.send(data);
+    })
+
 })
 
 
-router.get('/', (req,res) => {
-    let moderator = true;
-
-    if(req.params /*se ha un parametro tipo /users?name='mimmo' */)
-        return user.getModel().find({username: req.params /* .name */}) //ritorna una lista di users con quel nome
-    else if(moderator)
-        return user.getModel(); //intera tabella users
-})
 
 export = router;
 
