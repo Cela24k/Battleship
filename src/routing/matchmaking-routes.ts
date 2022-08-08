e2c9677f2af8df944284ea815c2399e4aa6b3ab4
Mimiview <882483@stud.unive.it>
2022-08-08 02:29:17 +0200

    remove ticket added

--- src/routing/matchmaking-routes.ts
+++ src/routing/matchmaking-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTicket } from "../models/ticket-entry";
+import { createTicket, removeTicket } from "../models/ticket-entry";
 
 export const router = Router();
 //TODO need to be tested
@@ -9,6 +9,19 @@ router.post('/join', async (req, res)=>{
         const ticket = await createTicket(userId);
         //TODO here must be the main swag feat. We need to cereate an Interval that begins the matchmaking phase. We can create some helper functions for the logic of searching an opponent and when it's finded we should emit in the socket the searchingmatch.
         return res.status(200).json(ticket);
+    }catch(err){
+        if (err === 'Server error')
+        return res.status(500).json({ error: true, errormessage: err, timestamp: Date.now() });
+    else
+        return res.status(404).json({ error: true, errormessage: err, timestamp: Date.now() });
+    }
+})
+
+router.delete('/remove', async (req,res)=>{
+    const userId = req.body.userId;
+    try {
+        await removeTicket(userId);
+        return res.status(200).json("Ticket removed from the matchamking queue.");
     }catch(err){
         if (err === 'Server error')
         return res.status(500).json({ error: true, errormessage: err, timestamp: Date.now() });
