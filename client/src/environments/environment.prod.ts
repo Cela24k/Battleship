ea45ee1f8d0ba06d7a7b1bbf241ebecfc418c304
Cela24k <882778@stud.unive.it>
2022-09-28 01:11:48 +0200

    added firebase

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-154-186-103.eu-west-1.compute.amazonaws.com'
+  apiUrl: 'http://ec2-54-154-186-103.eu-west-1.compute.amazonaws.com',
+  socketUrl: 'ws://ec2-54-154-186-103.eu-west-1.compute.amazonaws.com:8080'
+
 };
